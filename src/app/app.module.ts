2e02f2f10c805044bf727500bcb302db7561573c
Bob-development <proinvoke228@gmail.com>
2024-02-08 01:35:47 +0200

    add base marking

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
